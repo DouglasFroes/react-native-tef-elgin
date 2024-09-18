d2fd4b574d1ef61c9f13723434190b30002bf5b9
Douglas Froes <douglassantosfroes@gmail.com>
2024-09-18 16:47:10 -0300

    fix: create tef elgin native

--- android/src/main/java/com/tefelgin/TefElginModule.kt
+++ android/src/main/java/com/tefelgin/TefElginModule.kt
@@ -1,25 +1,114 @@
 package com.tefelgin
 
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReactContext
+
+import com.elgin.e1.pagamentos.tef.*
+
+import android.os.Handler
+import android.app.Activity
+import android.os.Message
+import android.os.Looper
+
+enum class TefWhat(val value: Int) {
+    PROGRESS(1),
+    COLLECT(2),
+    TRANSACTION(3),
+    INFO(4),
+    FINISH(5),
+    INFOPIX(6)
+}
 
 class TefElginModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
+  val context = reactContext as ReactContext
 
   override fun getName(): String {
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
-  }
-
   companion object {
     const val NAME = "TefElgin"
+  }
+
+  @ReactMethod
+  fun onInitTef() {
+    val activity: Activity = getCurrentActivity()!!
+
+    ElginTef.setContext(activity)
+    ElginTef.setHandler(mHandler)
+  }
+
+  @ReactMethod
+  fun configTef(name: String, version: String, pinpad: String, doc: String) {
+    ElginTef.InformarDadosAutomacao(name, version, pinpad,  "", "", "")
+
+    try{
+      ElginTef.AtivarTerminal(doc)
+    }catch(e: Exception){
+      val params: WritableMap = Arguments.createMap()
+      params.putString("message", e.message)
+      sendEventErro(params)
+    }
+  }
+
+  @ReactMethod
+  fun payDeb(value: String) {
+    try{
+      ElginTef.RealizarTransacaoDebito(value)
+    }catch(e: Exception){
+      val params: WritableMap = Arguments.createMap()
+      params.putString("message", e.message)
+      sendEventErro(params)
+    }
+  }
+
+  @ReactMethod
+  fun payCred(value: String, type: String, instalments: String) {
+    try{
+      ElginTef.RealizarTransacaoCredito(value, type, instalments)
+    }catch(e: Exception){
+      val params: WritableMap = Arguments.createMap()
+      params.putString("message", e.message)
+      sendEventErro(params)
+    }
+  }
+
+  @ReactMethod
+  fun payPix(value: String) {
+    try{
+      ElginTef.RealizarTransacaoPIX(value)
+    }catch(e: Exception){
+      val params: WritableMap = Arguments.createMap()
+      params.putString("message", e.message)
+      sendEventErro(params)
+    }
+  }
+
+  @ReactMethod
+  fun onCancel(){
+    try{
+      ElginTef.RealizarCancelamentoOperacao()
+    }catch(e: Exception){
+      val params: WritableMap = Arguments.createMap()
+      params.putString("message", e.message)
+      sendEventErro(params)
+    }
+  }
+
+  private fun sendEvent(params: WritableMap?) {
+    context
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+      .emit("ideploy.tef", params)
+  }
+
+  private fun sendEventErro(params: WritableMap?) {
+    context
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+      .emit("ideploy.tef.erro", params)
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,46 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
-const TefElgin = NativeModules.TefElgin
-  ? NativeModules.TefElgin
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+// eslint-disable-next-line prettier/prettier
+const TefElgin = NativeModules.TefElgin ? NativeModules.TefElgin : new Proxy({}, { get() { throw new Error(LINKING_ERROR); }, });
 
-export function multiply(a: number, b: number): Promise<number> {
-  return TefElgin.multiply(a, b);
+export function onInitTef(): void {
+  TefElgin.onInitTef();
+}
+
+type IConfigTef = {
+  name: string;
+  version: string;
+  pinpad: string;
+  doc: string;
+};
+
+export function configTef({ name, version, pinpad, doc }: IConfigTef): void {
+  TefElgin.configTef(name, version, pinpad, doc);
+}
+
+export function payDeb(value: number): void {
+  let valueToString = (value / 100).toFixed(2).toString();
+
+  TefElgin.payDeb(valueToString);
+}
+
+export function payCred(
+  value: number,
+  type: string,
+  installments: string
+): void {
+  let valueToString = (value / 100).toFixed(2).toString();
+
+  TefElgin.payCred(valueToString, type, installments);
+}
+
+export function payPix(value: number): void {
+  let valueToString = (value / 100).toFixed(2).toString();
+
+  TefElgin.payPix(valueToString);
+}
+
+export function onCancel(): void {
+  TefElgin.onCancel();
 }
